8c4bb55e3bed2bf9a87be0b892cd711d572048cf
Djemy <38529443+Djemy-Liem@users.noreply.github.com>
2022-03-27 06:40:04 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
                 </Layout>
                 <div className='footer'>
                     <Typography.Title level={5} style={{ color: 'white', textAlign: 'center'}}>
-                        Cryptalytics <br />
+                        Indo Crypto Info <br />
                         All rights reserved
                     </Typography.Title>
                     <Space>
